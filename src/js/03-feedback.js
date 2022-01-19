2acd96b642ca0d6b99b8940d9f0deefc244e8c1b
ololosh-tralevich <karpeko76@gmail.com>
2022-01-19 17:17:33 +0200

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,11 +8,9 @@ function checkLocalStorageData() {
   let userData = JSON.parse(localStorage.getItem('feedback-form-state'));
   try {
     if (userData.email || userData.password) {
-        userEmail.value = userData.email;
-        userMessage.value = userData.password;
+      userEmail.value = userData.email;
+      userMessage.value = userData.password;
     }
-    console.log(userData.email);
-    console.log(userData.password);
   } catch (error) {
     // logMyErrors(error);
   }
@@ -29,21 +27,23 @@ userEmail.addEventListener(
   throttle(event => {
     dataObject.email = event.currentTarget.value;
     localStorage.setItem('feedback-form-state', JSON.stringify(dataObject));
-  }, 0),
+  }, 500),
 );
 
 userMessage.addEventListener(
   'input',
   throttle(event => {
     dataObject.password = event.currentTarget.value;
+    // console.log(event.currentTarget.value);
     localStorage.setItem('feedback-form-state', JSON.stringify(dataObject));
-  }, 0),
+  }, 500),
 );
 
 userForm.addEventListener('submit', event => {
   event.preventDefault();
-  userEmail.value = "";
-  userMessage.value = "";
-  console.log(localStorage);
+  userEmail.value = '';
+  userMessage.value = '';
+
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   localStorage.clear();
 });
